8205119c0c33cf9f413f12e9d30de825e4ec7cdc
ariebesehl <a.riebesehl@gmail.com>
2025-09-24 17:56:25 +0200

    code/tcal: replaced indentation <space> with <tab>

--- ZMD5.c
+++ ZMD5.c
@@ -8,12 +8,12 @@
 #include "ZMD5-core.h"
 
 #if defined(__WIN32) || defined(_WIN32) || defined(WIN32) || defined(__WIN64) || defined(_WIN64) || defined(WIN64)
-    #include <io.h>
-    #include <fcntl.h>
-    #define ZMD5_BINARY_STDIN() setmode(STDIN_FILENO, O_BINARY)
-    #define ZMD5_WINDOWS
+	#include <io.h>
+	#include <fcntl.h>
+	#define ZMD5_BINARY_STDIN() setmode(STDIN_FILENO, O_BINARY)
+	#define ZMD5_WINDOWS
 #else
-    #define ZMD5_BINARY_STDIN()
+	#define ZMD5_BINARY_STDIN()
 #endif // OS MULTIPLEXER
 
 #define ZMD5_BUFFER_SIZE (1048576 * 2)
@@ -27,9 +27,9 @@ static unsigned char gBuffer[ZMD5_BUFFER_SIZE];
 #define ZMD5_MSG_READ		"wrn: could not open %d path(s)"
 //#define ZMD5_MSG_MOTD		ZMD5_MSG_NAME " -- " ZMD5_MSG_VERSION " -- " ZMD5_MSG_AUTHOR " -- " ZMD5_MSG_HELP
 #ifdef ZMD5_WINDOWS
-    #define ZMD5_MSG_LIST	"%s\t%I64u\t%s"
+	#define ZMD5_MSG_LIST	"%s\t%I64u\t%s"
 #else
-    #define ZMD5_MSG_LIST	"%s\t%llu\t%s"
+	#define ZMD5_MSG_LIST	"%s\t%llu\t%s"
 #endif // ZMD5_WINDOWS
 
 // ...I'm running this through a buffer instead of reading (and crunching) straight from the file descriptor, right?
@@ -40,9 +40,9 @@ void gZMD5_Hash(FILE* iFile) {
 	ZMD5_Finish();
 }
 int main(int iArgC, char** iArgV) {
-    int lError = 0;
+	int lError = 0;
 	#ifdef ZMD5_MSG_MOTD
-    fprintf(stderr, ZMD5_MSG_MOTD "\n");
+	fprintf(stderr, ZMD5_MSG_MOTD "\n");
 	#endif // ZMD5_MSG_MOTD
 	if (iArgC > 1) {
 		for (int i = 1; i < iArgC; ++i) {
@@ -64,5 +64,5 @@ int main(int iArgC, char** iArgV) {
 		gZMD5_Hash(stdin);
 		printf(ZMD5_MSG_LIST "\n", ZMD5_Digest(), ZMD5_Size(), "<stdin>");
 	}
-    return 0;
+	return 0;
 }
